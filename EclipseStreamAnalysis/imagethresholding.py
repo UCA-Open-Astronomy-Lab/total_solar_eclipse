80fb32750d64199e118bfa695eac08cf06537a52
August Childress <august.childress@yahoo.com>
2024-10-03 13:28:44 -0500

    changed the code to pixelcount thresholded frames, add them to a list, and display them on a graph over time. DO NOT RUN, WILL CRASH YOUR COMPUTER.

--- EclipseStreamAnalysis/imagethresholding.py
+++ EclipseStreamAnalysis/imagethresholding.py
@@ -10,12 +10,55 @@ import skimage.io
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-#test
-# get filename, sigma, and threshold value from command line
-filename = "EclipseStreamAnalysis/frame1.png"
+#setting up test storage for pixel count info inital date = 2460409.2186657293
+SecondToJD = 1.1574e-5
+t0 = 2460409.2186657293
+time = []#time in julian date of each pixel count
+pixelcount = []#pixelcount of frame with mask
+
+#sigma, and threshold value
 sigma = float(1.0) #controls how "Blurred" an image is, lower is clearer
 t = float(1.0)#Base threshold value
 t_rescaled = 0.95 #rescaled thrshold value gathered from grayscale.py
+
+for l in range(1,21):
+    #get filename
+    filename = "EclipseStreamAnalysis/capturedFrames/frame"+str(l)+".png"
+
+    # read and display the original image
+    image = mpimg.imread(filename)
+
+    # blur and grayscale before thresholding
+    blur = skimage.color.rgb2gray(image)
+    blur = skimage.filters.gaussian(blur, sigma=2)
+
+    # perform inverse binary thresholding
+    mask = blur > t_rescaled #it is greater than because we want white pixels
+
+    # use the mask to select the "interesting" part of the image
+    sel = np.zeros_like(image)
+    sel[mask] = image[mask]
+
+    #count pixels
+    sum = 0
+    for i in range(len(sel[0:,0,0])):
+        for j in range(len(sel[0,0:,0])):
+            for k in range(len(sel[0,0,0:])):
+                if sel[i,j,k] > 0:
+                    sum += 1
+                    break
+    print(l, ": ",sum)
+    pixelcount.append(sum)
+    time.append(t0 + (l-1)*SecondToJD)
+
+plt.plot(time, pixelcount, label = "Pixel Count")
+plt.title("2024-04-08 Total Solar Eclipse, Conway, AR 1429.25 MHz")
+plt.xlabel('JD Time')
+plt.ylabel('# of Pixels >= 0.95')
+plt.show()
+"""
+#get filename
+filename = "EclipseStreamAnalysis/capturedFrames/frame"+str(1)+".png"
 
 # read and display the original image
 image = mpimg.imread(filename)
@@ -32,10 +75,7 @@ mask = blur > t_rescaled #it is greater than because we want white pixels
 sel = np.zeros_like(image)
 sel[mask] = image[mask]
 
-# display the result
-imgplot = plt.imshow(sel)
-#plt.show()
-
+#count pixels
 sum = 0
 for i in range(len(sel[0:,0,0])):
     for j in range(len(sel[0,0:,0])):
@@ -43,4 +83,5 @@ for i in range(len(sel[0:,0,0])):
             if sel[i,j,k] > 0:
                 sum += 1
                 break
-print(sum)
+print(sum)"""
+
--- EclipseStreamAnalysis/pixelcounttest.py
+++ EclipseStreamAnalysis/pixelcounttest.py
@@ -12,9 +12,9 @@ img = Image.open("EclipseStreamAnalysis/capturedFrames/frame1.png")
 numpydata = asarray(img)   
 # data  
 ##print("Number of non zero pixels: ",np.count_nonzero(numpydata))
-imgplot = plt.imshow(numpydata)
+#imgplot = plt.imshow(numpydata)
 #plt.show()
-
+'''
 sum = 0
 for i in range(len(numpydata[0:,0,0])):
     for j in range(len(numpydata[0,0:,0])):
@@ -22,4 +22,5 @@ for i in range(len(numpydata[0:,0,0])):
             if numpydata[i,j,k] > 0:
                 sum += 1
                 break
-print(sum)
+print(sum)'''
+print(numpydata[350,650])
