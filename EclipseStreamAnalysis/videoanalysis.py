24edd71c3e1cc706b3b611005ac18661789455fd
August Childress <august.childress@yahoo.com>
2024-10-03 13:20:47 -0500

    Added code to filter the video to 1 fps and to take 20 frames total.

--- EclipseStreamAnalysis/videoanalysis.py
+++ EclipseStreamAnalysis/videoanalysis.py
@@ -8,7 +8,8 @@ end_time = "01:57:10"
 crop_dimensions = '1300:1300:1300:500' #sets values for cropping, from left to right: Width of cropped video, height of cropped video, top left x coordinate of cropped video, top left y coordinate for cropped video
 (
     ffmpeg.input(eclipse_mp4, ss=start_time, to=end_time)#the video we want to extract data from
-    .filter('crop', *crop_dimensions.split(':'))#crops the video
-    .output('EclipseStreamAnalysis/capturedFrames/frame%d.png',vframes = 1)# extracts frames from the entire video at 1 fps and saves them as pngs
+    .filter('crop', *crop_dimensions.split(":"))#crops the video
+    .filter('fps', fps=1, round='up')
+    .output('EclipseStreamAnalysis/capturedFrames/frame%d.png')# extracts frames and saves them as pngs
     .run()
 )
